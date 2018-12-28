d6faf35247e69514c665a558982dd12950d7ce72
powell956 <powell956@gmail.com>
2018-12-28 03:10:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,27 @@
+# def reverse_each_word(string)
+#
+#   words = string.split
+#   new_words = []
+#
+#   words.each do |word|
+#     letters = word.split("")
+#     letters.reverse!
+#     new_word = letters.join
+#     new_words << new_word
+#   end
+#
+#   new_words.join(" ")
+# end
+
 def reverse_each_word(string)
 
   words = string.split
-  new_words = []
 
-  words.each do |word|
+  words.collect do |word|
     letters = word.split("")
     letters.reverse!
     new_word = letters.join
-    new_words << new_word
   end
 
-  new_words.join(" ")
+  string
 end
